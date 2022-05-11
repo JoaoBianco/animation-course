14556026dd147863706e5080f5bf59972bf464ef
JoaoBianco <joaops.bianco@gmail.com>
2022-05-11 10:07:02 -0300

    Finish cookie animation

--- Cookie Popup Starter/app.js
+++ Cookie Popup Starter/app.js
@@ -1,5 +1,6 @@
 const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
 
+//Timeline
 tl.fromTo(
   ".cookie-container",
   { scale: 0 },
@@ -13,6 +14,32 @@ tl.fromTo(
 );
 tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
 
+//Cookie jump
+tl.fromTo(
+  ".cookie",
+  { y: 0, rotation: 0 },
+  { y: -10, rotation: -10, yoyo: true, repeat: -1 }
+);
+
+tl.fromTo(
+  "#crumbs",
+  { rotation: 0 },
+  { rotation: -10, yoyo: true, repeat: -1 },
+  "<"
+);
+
+tl.fromTo(
+  "button",
+  { scale: 1 },
+  {
+    scale: 1.05,
+    yoyo: true,
+    repeat: -1,
+    delay: 10,
+  }
+);
+
+//Fading the cookie out
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
