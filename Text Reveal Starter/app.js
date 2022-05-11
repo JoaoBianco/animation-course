8568879ede246d7c51ae65fa7a29bbc4b3bd22be
JoaoBianco <joaops.bianco@gmail.com>
2022-05-11 10:56:30 -0300

    Finish text animation

--- Text Reveal Starter/app.js
+++ Text Reveal Starter/app.js
@@ -20,3 +20,20 @@ tl.fromTo(
   { scale: 1, opacity: 1, ease: "elastic.out(1.5,1)", duration: 1 },
   "<"
 );
+
+//split text alternative
+const logo = document.querySelector(".logo");
+const letters = logo.textContent.split("");
+
+logo.textContent = "";
+
+letters.forEach((letter) => {
+  logo.innerHTML += `<span class="letter">${letter}</span>`;
+});
+
+gsap.set(".letter", { display: "inline-block" });
+gsap.fromTo(
+  ".letter",
+  { y: "100%" },
+  { y: 0, delay: 2, stagger: 0.05, ease: "back.out(3)" }
+);
