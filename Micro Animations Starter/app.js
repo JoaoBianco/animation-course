73c27967a8a1002646c20b07ee703c53aa4f06d5
JoaoBianco <joaops.bianco@gmail.com>
2022-05-24 10:05:32 -0300

    Notification animation

--- Micro Animations Starter/app.js
+++ Micro Animations Starter/app.js
@@ -3,6 +3,7 @@ const tl = gsap.timeline({
 });
 
 const home = document.querySelector(".home");
+const notification = document.querySelector(".notifications");
 
 gsap.set(".feather", { scale: 0, transformOrigin: "center" });
 home.addEventListener("click", () => {
@@ -13,4 +14,26 @@ home.addEventListener("click", () => {
     { y: 20, scale: 1.5, duration: 1, stagger: 0.2 }
   );
   gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
+});
+
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "center" });
+gsap.set(".wave", { transformOrigin: "bottom" });
+gsap.set(".wave", { opacity: 0 });
+notification.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, duration: 2, ease: "elastic.out(5,0.2)" }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -3, x: 0.5 },
+    { rotation: 0, x: 0, duration: 2, ease: "elastic.out(5,0.2)" }
+  );
+  gsap.fromTo(
+    ".wave",
+    { opacity: 1, scale: 0.7 },
+    { opacity: 0, scale: 1.2, duration: 1 }
+  );
 });
